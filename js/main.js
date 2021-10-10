0b5276a6606bc29008e04774aaa31699e491b110
Алексей Райц <832cms@gmail.com>
2021-10-10 12:31:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer', 
+  'elevator', 
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow', 
+  'hotel', 
+];
+
+const TIME_IN_OUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
 const randomInt = (min, max) => {
   if (min < 0 || max < 0 || min > max) {
     return('Use correct values!');
@@ -11,13 +40,82 @@ const randomFloat = (min, max, decimalPlaces) => {
   if (min < 0 || max < 0 || min > max) {
     return('Use correct values!');
   } else {
-    const assistantNumber = Math.pow(10, decimalPlaces);
-    const rand = min + Math.random() * (max - min);
-    return Math.ceil(rand*assistantNumber-1)/assistantNumber;
+    const rand = Math.random() * (max - min) + min;
+    return rand.toFixed(decimalPlaces);
   }
 };
 
-randomInt(3, 7);
-randomFloat(1.3, 2.81, 3);
+const uniqueRandomArr = (elements) => {
+  let newArr = [];
+  const arrLength = _.random(0, elements.length-1);
+  let currentValue = getRandomArrayElement(elements);
+  while (arrLength !== newArr.length-1) {
+    while (newArr.includes(currentValue)) {
+      currentValue = getRandomArrayElement(elements);
+    }
+    newArr.push(currentValue);
+  }
+  return newArr;
+};
+
+const leadingZero = (index) => {
+  if (index < 10) {
+    index = '0' + index;
+  }
+  return index.toString();
+};
+
+const getRandomArrayElement = (elements) => {
+  return elements[_.random(0, elements.length-1)];
+}
+
+const createAuthor = (index) => {
+  return {
+    avatar: 'img/avatars/user' + leadingZero(index) + '.png',  
+  };
+};
+
+const createOffer = (location) => {
+  return {
+    title: "Тестовый заголовок",
+    address: location.lat + ' ,' + location.lng,
+    price: randomInt(23000, 75000),
+    type: getRandomArrayElement(TYPES),
+    rooms: randomInt(1,9),
+    guests: randomInt(1,5),
+    checkin: getRandomArrayElement(TIME_IN_OUT),
+    checkout: getRandomArrayElement(TIME_IN_OUT),
+    features: uniqueRandomArr(FEATURES),
+    description: "Тестовое описание",
+    photos: uniqueRandomArr(PHOTOS), 
+  };
+};
+
+const createLocation = () => {
+  return {
+    lat: randomFloat(35.65000, 35.70000, 5),  
+    lng: randomFloat(139.70000, 139.80000, 5),
+  };
+};
+
+//Создаем объявление
+const createAnnouncement = (index) => {
+  
+  const location = createLocation();
+
+  return {
+    author: createAuthor(index),
+    offer: createOffer(location),
+    location: location,
+  };
+};
+
+const arrAnnouncements = [];
+for (let i = 1; i <= 10; i++) {
+  arr.push(createAnnouncement(i))
+}
+
+console.log(arrAnnouncements);
+
 
 
